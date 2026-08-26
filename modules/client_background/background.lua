288d0deb682b8b603fe2727fd82d42915e0d70f0
Podkarpacie <podkarpacie@users.noreply.github.com>
2026-08-26 20:51:42 +0200

    Forgotten Client 7.4: modernized Midgard fork rebranded for Forgotten Engine
    - Rebranded from Midgard to Forgotten Client 7.4 (app name, solution, build config)
    - Native 740 protocol, chat working at 740 (classic message path)
    - Modernized for VS2022 v143 + Boost 1.92 Asio/C++17 + OpenSSL 3 + vcpkg deps
    - Default login server 127.0.0.1:7172 protocol 740
    - MIT-licensed; retains OTClient/Midgard attribution

---
+++ modules/client_background/background.lua
@@ -0,0 +1,32 @@
+-- private variables
+local background
+
+-- public functions
+function init()
+  background = g_ui.displayUI('background')
+  background:lower()
+
+  connect(g_game, { onGameStart = hide })
+  connect(g_game, { onGameEnd = show })
+end
+
+function terminate()
+  disconnect(g_game, { onGameStart = hide })
+  disconnect(g_game, { onGameEnd = show })
+
+  background:destroy()
+
+  Background = nil
+end
+
+function hide()
+  background:hide()
+end
+
+function show()
+  background:show()
+end
+
+function getBackground()
+  return background
+end
---
+++ src/main.cpp
@@ -0,0 +1,63 @@
+﻿/*
+ * Forgotten Client 7.4
+ *
+ * Derived from Midgard (https://github.com/sp4wna1/Midgard-Client), a fork of
+ * OTClient (https://github.com/edubart/otclient). MIT-licensed (see LICENSE).
+ *
+ * Copyright (c) 2019 Midgard <https://github.com/sp4wna1/Midgard-Client>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <framework/core/application.h>
+#include <framework/core/resourcemanager.h>
+#include <framework/luaengine/luainterface.h>
+#include <client/client.h>
+
+int main(int argc, const char* argv[])
+{
+    std::vector<std::string> args(argv, argv + argc);
+
+    // setup application name and version
+    g_app.setName("Forgotten Client");
+    g_app.setCompactName("forgottenclient");
+    g_app.setVersion(VERSION);
+
+    // initialize application framework and forgotten client
+    g_app.init(args);
+    g_client.init(args);
+
+    // find script init.lua and run it
+    if(!g_resources.discoverWorkDir("init.lua"))
+        g_logger.fatal("Unable to find work directory, the application cannot be initialized.");
+
+    if(!g_lua.safeRunScript("init.lua"))
+        g_logger.fatal("Unable to run script init.lua!");
+
+    // the run application main loop
+    g_app.run();
+
+    // unload modules
+    g_app.deinit();
+
+    // terminate everything and free memory
+    g_client.terminate();
+    g_app.terminate();
+    return 0;
+}
